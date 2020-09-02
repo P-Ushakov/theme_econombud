886eba808edfc029ef4e08d3527d8a0f6b4f3326
Pavel Ushakov <p.v.ushakov@gmail.com>
2020-09-02 19:02:57 +0300

    correct hamburger menu and some othe bugs

---
+++ static/src/js/ekonombud_phonet.js
@@ -0,0 +1,25 @@
+//EkonombudPhonetScript
+odoo.define("website.user_custom_code", function (require) {
+    "use strict";
+  
+    //var Dialog = require('web.Dialog');
+    var publicWidget = require("web.public.widget");
+    /* var telerWdWidgetId="de021028-c353-42eb-b69c-6417e936bd03"
+       var telerWdDomain="ekonombud.phonet.com.ua" */
+  
+    publicWidget.registry.EkonombudPhonetScript = publicWidget.Widget.extend({
+      selector: "#wrapwrap",
+  
+      start: function () {
+        window.telerWdWidgetId="de021028-c353-42eb-b69c-6417e936bd03"
+        window.telerWdDomain="ekonombud.phonet.com.ua"
+
+        let phonetScript = document.createElement("script");
+        phonetScript.setAttribute("src", "//ekonombud.phonet.com.ua/public/widget/call-catcher/lib-v3.js");
+        document.body.appendChild(phonetScript);        
+  
+        return this._super.apply(this, arguments);
+      },
+    });
+  });
+  
---
+++ static/src/js/hamburger_animation.js
@@ -0,0 +1,27 @@
+//HAMBURGER
+odoo.define("website.user_custom_code", function (require) {
+  "use strict";
+
+  //var Dialog = require('web.Dialog');
+  var publicWidget = require("web.public.widget");
+
+  publicWidget.registry.hamburgerAnimation = publicWidget.Widget.extend({
+    selector: "#wrapwrap",
+
+    start: function () {
+      let burgers = document.getElementsByClassName("navbar-toggler");
+      //let burger = burgers.item(0)
+
+      for (let burger of burgers) {
+        let burgerSecondSpan = document.createElement("SPAN");
+        let burgerThirdSpan = document.createElement("SPAN");
+        burger.classList.add("collapsed");
+        burger.appendChild(burgerSecondSpan);
+        burger.appendChild(burgerThirdSpan);
+        //console.log("span added")
+      }
+
+      return this._super.apply(this, arguments);
+    },
+  });
+});
