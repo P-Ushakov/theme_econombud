3b7e380296e798ba8338aaa912fff21249375ecd
Pavel Ushakov <p.v.ushakov@gmail.com>
2020-09-03 21:35:01 +0300

    add data to painting page

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Econombud theme",
-    'version': '1.1',
+    'version': '1.2',
     'summary': 'Econombud custom starter theme',
     'description': """
 Author: P.Ushakov 
